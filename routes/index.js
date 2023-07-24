6b187a7900512cc0ebeda95915c6b53794604df0
SeungJong Ha <hippo0419@kaist.ac.kr>
2023-07-24 16:41:35 +0000

    feat(ObjDetection): 사진 촬영 및 저장 기능 구현

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 const test = require("./test");
+const objectDetection = require("./ObjectDetection");
 
 router.get("/", (req, res) => {
   res.locals.title = "Node Chat!";
   res.json("{index}");
 });
 router.use("/test", test);
+router.use("/object-detection", objectDetection);
 
 module.exports = router;
