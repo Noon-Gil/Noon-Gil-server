44c445ad9b8bac62d59c96aa801205a048ae90a8
SeungJong Ha <hippo0419@daum.net>
2023-07-26 10:04:37 +0000

    feat: stt/selection api 추가

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const test = require("./test");
 const objectDetection = require("./ObjectDetection");
+const soundToText = require("./SountToText");
 const ocr = require("./OCR");
 
 router.get("/", (req, res) => {
@@ -13,6 +14,7 @@ router.get("/", (req, res) => {
 
 router.use("/test", test);
 router.use("/object-detection", objectDetection);
+router.use("/stt", soundToText);
 router.use("/ocr", ocr);
 
 module.exports = router;
